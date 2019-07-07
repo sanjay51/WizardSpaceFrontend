ef9ed3d8507e50e947d7551283539813476efb50
Sanjay Verma <sanjav@amazon.com>
2019-07-07 12:10:46 -0700

    Configure Login success and cancel routes

--- src/app/user/login/login.component.ts
+++ src/app/user/login/login.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { ROUTE_HOME } from '../../constants';
 
 @Component({
   selector: 'app-login',
@@ -6,6 +7,9 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./login.component.css']
 })
 export class LoginComponent implements OnInit {
+  loginSuccessRoute = ROUTE_HOME;
+  loginCancelRoute = ROUTE_HOME;
+
   constructor(
   ) { }
 
