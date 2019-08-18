143cf828a1e41cef649c819b1d3aea3b0caeb0e5
Sanjay Verma <sanjayvermaseattle@gmail.com>
2019-08-18 09:31:15 -0700

    Update app-settings-form component

--- src/app/app-editor/app-settings-form/app-settings-form.component.ts
+++ src/app/app-editor/app-settings-form/app-settings-form.component.ts
@@ -22,31 +22,52 @@ export class AppSettingsFormComponent implements OnInit {
     fields: [
       {
         name: "title",
-        label: "App Title",
+        label: "App Name/Title*",
         validators: [Validators.required, Validators.minLength(6)],
         defaultValue: '',
       },
       {
         name: "description",
-        label: "Description",
+        type: "textarea",
+        label: "Description*",
         validators: [Validators.required],
         defaultValue: ''
       },
       {
         name: "category",
-        label: "Category",
+        type: "select",
+        label: "Category*",
+        options: ["Art & Design", "Auto & Vehicles", "Beauty", "Books & Reference", "Business", "Comics", "Communications", "Dating", "Education", "Entertainment", "Events", "Finance", "Food & Drink", "Health & Fitness", "House & Home", "Libraries & Demo", "Lifestyle", "Maps & Navigation", "Medical", "Music & Audio", "News & Magazines", "Parenting", "Personalization", "Photography", "Productivity", "Shopping", "Social", "Sports", "Tools", "Travel & Local", "Video Players & Editors", "Weather"],
+        validators: [Validators.required],
+        defaultValue: 'Productivity'
+      },
+      {
+        name: "logo",
+        label: "Logo URL*",
         validators: [Validators.required],
         defaultValue: ''
       },
       {
-        name: "icon",
-        label: "Icon",
+        name: "screenshot-1",
+        label: "Screenshot-1 URL*",
+        validators: [Validators.required],
+        defaultValue: ''
+      },
+      {
+        name: "screenshot-2",
+        label: "Screenshot-2 URL",
         validators: [],
         defaultValue: ''
       },
       {
-        name: "screenshots",
-        label: "Screenshots",
+        name: "screenshot-3",
+        label: "Screenshot-3 URL",
+        validators: [],
+        defaultValue: ''
+      },
+      {
+        name: "video",
+        label: "Video URL (optional)",
         validators: [],
         defaultValue: ''
       }
