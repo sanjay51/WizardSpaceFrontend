60f6c49c43dfbe9ebd531f3438e348908cf2fc61
Sanjay Verma <sanjayvermaseattle@gmail.com>
2019-08-18 22:22:45 -0700

    Publish-app-modal: updates

--- src/app/app-editor/publish-app-modal/publish-app-modal.component.ts
+++ src/app/app-editor/publish-app-modal/publish-app-modal.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { AuthenticationService } from 'ix-angular-elements';
 import { AppStateService } from '../../app-state.service';
 
 @Component({
@@ -7,13 +8,31 @@ import { AppStateService } from '../../app-state.service';
   styleUrls: ['./publish-app-modal.component.scss']
 })
 export class PublishAppModalComponent implements OnInit {
+  screen = Screen.INITIAL;
+  Screen = Screen;
 
-  constructor(private appState: AppStateService) { }
+  constructor(private appState: AppStateService, private authentication: AuthenticationService) { }
 
   ngOnInit() {
+  }
+
+  close() {
+    this.setModalVisibility(false);
+    this.screen = Screen.INITIAL;
+  }
+
+  initialScreenContinue() {
+    if (this.authentication.isLoggedIn()) {
+
+    }
+    this.screen = Screen.MUST_LOGIN;
   }
 
   setModalVisibility(isVisible: boolean) {
     this.appState.isPublishAppModalVisible = isVisible;
   }
+}
+
+enum Screen {
+  INITIAL, MUST_LOGIN, LOADING_APP_INFO, PUBLISH, SUCCESS
 }
