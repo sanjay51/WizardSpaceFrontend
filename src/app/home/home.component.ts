0e4e59c06085660e2ade7359e99e19efa1a6bf57
Sanjay Verma <sanjayvermaseattle@gmail.com>
2019-09-02 19:41:41 -0700

    homepage - card layout should wrap

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -11,13 +11,6 @@ import { GetAppsByGroupIdAPI } from '../get-apps-by-group.api';
 export class HomeComponent implements OnInit {
   apps: App[] = [];
   status = 'loading';
-  /*
-    new IxCard("image-gallery", "APP 1", "App 1 description", "link", "black", "white"),
-    new IxCard("ruler-pencil", "APP 2", "App 2 description", "link", "gray", "white"),
-    new IxCard("truck", "APP 3", "App 3 description", "link", "lightcyan", "black"),
-    new IxCard("line-chart", "APP 4", "App 4 description", "link", "lightgreen", "black"),
-    new IxCard("bullseye", "APP 5", "App 5 description", "link", "lightcoral", "black"),
-  */
 
   constructor(private state: AuthStateService, private apiService: APIService) {
   }
