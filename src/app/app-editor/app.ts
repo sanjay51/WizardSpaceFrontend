b10c9d920207db256ed5e37cd373c02f875bf39c
Sanjay Verma <sanjayvermaseattle@gmail.com>
2019-09-10 22:22:37 -0700

    Update active app categories

--- src/app/app-editor/app.ts
+++ src/app/app-editor/app.ts
@@ -22,7 +22,7 @@ export class App {
     isIOSInstallable: boolean;
     lighthouseScore: number;
 
-    public static fromAppGroupResponse(appData: any): App{
+    public static fromAppGroupResponse(appData: any): App {
         let app: App = new App();
 
         app.appId = this.get("appId", appData);
@@ -36,7 +36,7 @@ export class App {
         app.logo = this.get("logo", appData);
         app.video = this.get("video", appData);
         app.isExternal = this.get("isExternal", appData);
-    
+
         app.isHTTPSEnabled = this.get("isHTTPSEnabled", appData) == "true";
         app.isOfflineSupported = this.get("isOfflineSupported", appData) == "true";
         app.isAndroidInstallable = this.get("isAndroidInstallable", appData) == "true";
@@ -53,4 +53,49 @@ export class App {
     }
 }
 
-export const APP_CATEGORIES = ["Art & Design", "Auto & Vehicles", "Beauty", "Books & Reference", "Business", "Comics", "Communications", "Dating", "Education", "Entertainment", "Events", "Finance", "Food & Drink", "Games", "Health & Fitness", "House & Home", "Libraries & Demo", "Lifestyle", "Maps & Navigation", "Medical", "Music & Audio", "News & Magazines", "Parenting", "Personalization", "Photography", "Productivity", "Shopping", "Social", "Sports", "Tools", "Travel & Local", "Video Players & Editors", "Weather"];
+export const ACTIVE_APP_CATEGORIES = [
+    /*{ name: "Art & Design", "icon": "filter_vintage", "path": "" },*/
+    /*{ name: "Beauty", "icon": "face", "path": "" },*/
+    { name: "Books & Reference", "icon": "library_books", "path": "Reference" },
+    { name: "Business", "icon": "business", "path": "Business" },
+    /* { name: "Comics", "icon": "", "path": ""}, */
+    /* { name: "Communications", "icon": "chat", "path": "" },
+    { name: "Dating", "icon": "favorite", "path": "" }, */
+    { name: "Education", "icon": "school", "path": "Education" },
+    { name: "Entertainment", "icon": "movie", "path": "Entertainment" },
+    /* { name: "Events", "icon": "event", "path": "" },
+    { name: "Finance", "icon": "money", "path": "" }, */
+    { name: "Food & Drink", "icon": "fastfood", "path": "Food & Drink" },
+    { name: "Games", "icon": "sports_esports", "path": "Games" },
+    /* { name: "Health & Fitness", "icon": "fitness_center", "path": "" },
+    { name: "House & Home", "icon": "home", "path": "" },
+    { name: "Libraries & Demo", "icon": "library_books", "path": ""}, */
+    { name: "Lifestyle", "icon": "deck", "path": "Lifestyle" },
+    /* { name: "Maps & Navigation", "icon": "map", "path": "" },
+    { name: "Medical", "icon": "local_hospital", "path": "" }, */
+    { name: "Music & Audio", "icon": "music_video", "path": "Music" },
+    { name: "News & Magazines", "icon": "group_work", "path": "News" },
+    /* { name: "Parenting", "icon": "", "path": ""}, */
+    /* { name: "Personalization", "icon": "", "path": ""}, */
+    /* { name: "Photography", "icon": "insert_photo", "path": "" }, */
+    { name: "Productivity Tools", "icon": "waves", "path": "Tools" },
+    { name: "Shopping", "icon": "add_shopping_cart", "path": "Shopping" },
+    { name: "Social", "icon": "deck", "path": "Social" },
+    { name: "Sports", "icon": "sports_football", "path": "Sports" },
+    /* { name: "Tools", "icon": "", "path": ""}, */
+    { name: "Travel & Local", "icon": "location_city", "path": "Travel" },
+    /* { name: "Video Players & Editors", "icon": "", "path": ""}, */
+    /* { name: "Weather", "icon": "wb_sunny", "path": "" } */
+]
+
+export const APP_CATEGORIES = [
+    "Art & Design",
+    "Auto & Vehicles",
+    "Beauty", "Books & Reference",
+    "Business", "Comics", "Communications", "Dating",
+    "Education", "Entertainment", "Events", "Finance",
+    "Food & Drink", "Games", "Health & Fitness", "House & Home", "Libraries & Demo",
+    "Lifestyle", "Maps & Navigation", "Medical", "Music & Audio", "News & Magazines",
+    "Parenting", "Personalization", "Photography", "Productivity", "Shopping", "Social",
+    "Sports", "Tools", "Travel & Local", "Video Players & Editors", "Weather"
+];
--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,6 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { APIService, AuthStateService } from 'ix-angular-elements';
-import { App } from '../app-editor/app';
+import { App, ACTIVE_APP_CATEGORIES } from '../app-editor/app';
 import { GetAppsByGroupIdAPI } from '../get-apps-by-group.api';
 import { ActivatedRoute } from '@angular/router';
 import { GetAppsByGroupAndCategoryAPI } from './get-apps-by-group-and-category.api';
@@ -11,6 +11,7 @@ import { GetAppsByGroupAndCategoryAPI } from './get-apps-by-group-and-category.a
   styleUrls: ['./home.component.scss']
 })
 export class HomeComponent implements OnInit {
+  categories = ACTIVE_APP_CATEGORIES;
   apps: App[] = [];
   status = 'loading';
 
@@ -74,41 +75,6 @@ export class HomeComponent implements OnInit {
       this.status = 'error'
     });
   }
-
-  public categories = [
-    { name: "Art & Design", "icon": "filter_vintage", "path": "" },
-    { name: "Beauty", "icon": "face", "path": "" },
-    { name: "Books & Reference", "icon": "library_books", "path": "" },
-    { name: "Business", "icon": "business", "path": "" },
-    /* { name: "Comics", "icon": "", "path": ""}, */
-    { name: "Communications", "icon": "chat", "path": "" },
-    { name: "Dating", "icon": "favorite", "path": "" },
-    { name: "Education", "icon": "school", "path": "" },
-    { name: "Entertainment", "icon": "movie", "path": "" },
-    { name: "Events", "icon": "event", "path": "" },
-    { name: "Finance", "icon": "money", "path": "" },
-    { name: "Food & Drink", "icon": "fastfood", "path": "" },
-    { name: "Games", "icon": "sports_esports", "path": "" },
-    { name: "Health & Fitness", "icon": "fitness_center", "path": "" },
-    { name: "House & Home", "icon": "home", "path": "" },
-    /* { name: "Libraries & Demo", "icon": "library_books", "path": ""}, */
-    { name: "Lifestyle", "icon": "deck", "path": "" },
-    { name: "Maps & Navigation", "icon": "map", "path": "" },
-    { name: "Medical", "icon": "local_hospital", "path": "" },
-    { name: "Music & Audio", "icon": "music_video", "path": "" },
-    { name: "News & Magazines", "icon": "group_work", "path": "" },
-    /* { name: "Parenting", "icon": "", "path": ""}, */
-    /* { name: "Personalization", "icon": "", "path": ""}, */
-    { name: "Photography", "icon": "insert_photo", "path": "" },
-    { name: "Productivity Tools", "icon": "waves", "path": "" },
-    { name: "Shopping", "icon": "add_shopping_cart", "path": "" },
-    { name: "Social", "icon": "deck", "path": "" },
-    { name: "Sports", "icon": "sports_football", "path": "" },
-    /* { name: "Tools", "icon": "", "path": ""}, */
-    { name: "Travel & Local", "icon": "location_city", "path": "" },
-    /* { name: "Video Players & Editors", "icon": "", "path": ""}, */
-    { name: "Weather", "icon": "wb_sunny", "path": "" }
-  ]
 }
 
 /*
