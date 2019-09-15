a984d3558b37fdb8ab440ef3a91a352a18512920
Sanjay Verma <sanjayvermaseattle@gmail.com>
2019-09-14 23:59:28 -0700

    More carousal changes

--- src/app/home/app-carousal/app-carousal.component.ts
+++ src/app/home/app-carousal/app-carousal.component.ts
@@ -20,31 +20,31 @@ export class AppCarousalComponent implements OnInit {
       imageUrl: "https://wizardspace-images.s3.amazonaws.com/banner/google_santa_tracker.png",
       title: "Google Santa Tracker",
       backgroundColor: "#4edcf4",
-      appId: "f9c07420-8992-445b-a384-71a51e3f9004"
+      appId: "716eecc5-ca32-479f-ac9b-80f1fd2b67cb"
     },
     {
       imageUrl: "https://wizardspace-images.s3.amazonaws.com/banner/headline.png",
       title: "Headline",
       backgroundColor: "white",
-      appId: "f9c07420-8992-445b-a384-71a51e3f9004"
+      appId: "1a5327f0-a11f-4eae-b975-dca0597850e8"
     },
     {
       imageUrl: "https://wizardspace-images.s3.amazonaws.com/banner/telegram.png",
       title: "Telegram",
       backgroundColor: "#517fa3",
-      appId: "f9c07420-8992-445b-a384-71a51e3f9004"
+      appId: "3165535a-faf4-4696-848c-fac5b213ad2b"
     },
     {
       imageUrl: "https://wizardspace-images.s3.amazonaws.com/banner/twitter.jpg",
       title: "Twitter",
-      backgroundColor: "white",
-      appId: "f9c07420-8992-445b-a384-71a51e3f9004"
+      backgroundColor: "#32ccfe",
+      appId: "ffe00b9e-7423-47c9-b169-87513576229b"
     },
     {
       imageUrl: "https://wizardspace-images.s3.amazonaws.com/banner/tower_game.png",
       title: "The Tower Game",
       backgroundColor: "#66dada",
-      appId: "f9c07420-8992-445b-a384-71a51e3f9004"
+      appId: "34218761-10c4-4fbb-9552-52d77d9a588f"
     },
   ]
 
